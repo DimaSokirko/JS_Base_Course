7c2cd35a5f7fd8798a51b7283bc48a5e28c41a0e
DimaSokirko <92520052+DimaSokirko@users.noreply.github.com>
2021-11-19 19:40:39 +0200

    s

---
+++ lesson3/hw/js/task1.js
@@ -0,0 +1,77 @@
+//   1. Сделать собственные примеры методов применяемых для массивов.
+// 1 join
+
+var myArray = ["Banana", "Orange", "Apple", "Mango"];
+var joinMyArray = myArray.join();
+console.log(joinMyArray);
+
+// 2 reverse 
+
+var myArray = ["Banana", "Orange", "Apple", "Mango"];
+var reverseMyArray = myArray.reverse();
+console.log(reverseMyArray);
+
+// 3 sort
+
+var myArray = ["Banana", "Orange", "Apple", "Mango"];
+var sortMyArray = myArray.sort();
+console.log(sortMyArray);
+
+// 4 concat
+
+var myArray = ["Banana", "Orange", "Apple", "Mango"];
+var concatMyArray = myArray.concat(["Fruit"]);
+console.log(concatMyArray);
+
+// 5 slice
+
+var myArray = ["Banana", "Orange", "Apple", "Mango"];
+var sliceMyArray = myArray.slice(1,3);
+console.log(myArray);
+
+// 6 splice
+
+var myArray = ["Banana", "Orange", "Apple", "Mango"];
+var spliceMyArray = myArray.splice(0, 2, "Audi");
+console.log(myArray);
+
+// 7 push 
+
+var myArray = ["Banana", "Orange", "Apple", "Mango"];
+var pushMyArray = myArray.push("Dima", "Vova");
+console.log(myArray);
+
+// 8 unshift
+
+var myArray = ["Banana", "Orange", "Apple", "Mango"];
+var unshiftMyArray = myArray.unshift("Alla", "Irina");
+console.log(myArray);
+
+// 9 shift
+
+var myArray = ["Banana", "Orange", "Apple", "Mango"];
+var shiftMyArray = myArray.shift();
+console.log(myArray);
+
+// 10 pop 
+
+var myArray = ["Banana", "Orange", "Apple", "Mango"];
+var popMyArray = myArray.pop();
+console.log(myArray);
+
+// 11 split
+
+var myArray = "Banana, Orange, Apple, Mango";
+var splitMyArray = myArray.split(", ");
+console.log(splitMyArray);
+
+
+
+
+
+
+
+
+
+
+
